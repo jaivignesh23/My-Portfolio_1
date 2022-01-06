32cc9c9d16d2d374a01ca61c65b4c20d4df10f7c
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2022-01-06 18:13:37 -0500

    Include toggle | setup and default dark theme |  drive link

--- javascript/custom.js
+++ javascript/custom.js
@@ -37,3 +37,31 @@
     });
 
 })(jQuery);
+
+
+$(function() {
+    const d = new Date();
+    const hours = d.getHours();
+    const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+    const body = document.querySelector('body');
+    const toggle = document.getElementById('toggle');
+    const input = document.getElementById('switch');
+  
+    body.classList.add('night');
+    document.getElementById('switch').checked = true;
+
+    if (night) {
+      input.checked = true;
+      body.classList.add('night');
+    }
+  
+    toggle.addEventListener('click', function() {
+      const isChecked = input.checked;
+      if (isChecked) {
+        body.classList.remove('night');
+      } else {
+        body.classList.add('night');
+      }
+    });
+  
+  });
